8b4656a9fc647408d910382746263d663e81b095
Dialtor <diepresiga@gmail.com>
2021-10-07 21:56:13 -0500

    Implementando el Updated y el Delete de la tabla cliente

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,7 @@ useExpress.use(bodyParser.urlencoded({extended:true}));
 
 const Cliente = require('../models/Clientes');
 const Registro = require('../models/Registros');
-const { findByIdAndUpdate } = require('../models/Clientes');
+const findByIdAndUpdate = require('../models/Clientes');
 
 
 
@@ -26,76 +26,59 @@ const { findByIdAndUpdate } = require('../models/Clientes');
         res.render('Index');
     });
 
+    router.get('/ClientesRegistro', (req, res) =>{
+        // console.log(clientes);
+        res.render('ClientesRegistro')
+    });
+
+
+
+    //CONTROLADORES
+
+    //Controlador Registro
     router.get('/Clientes', async (req, res) =>{
         const clientes = await Cliente.find();
         res.render('Clientes', {clientes: clientes})
         // res.json(clientes);
     });
 
-    router.get('/ClientesRegistro', (req, res) =>{
-        // console.log(clientes);
-        res.render('ClientesRegistro')
-    });
+   
 
     router.get('/ClientesEditar/:id', async (req, res) =>{
-         const editar =  await Cliente.findById(req.params.id);
-        // console.log(clientes);
-        res.render('ClientesEditar', {editar: editar})
-        
-    });
+        const editar =  await Cliente.findById(req.params.id);
+       // console.log(clientes);
+       res.render('ClientesEditar', {editar: editar})
+       
+   });
 
-     router.post('/ClientesEditar/Guardar/:id', (req,res) => {
-         const idCliente = (req.params.id);
-         
-        
-         console.log(idCliente);
-         Cliente.findByIdAndUpdate(idCliente, {
-              nombre: req.body.nombreClienteEditar,
-              cedula: req.body.cedulaClienteEditar,
-              direccion: req.body.direccionClienteEditar,
-              celular: req.body.celularClienteEditar,
-              correo: req.body.correoClienteEditar
-         }, (error, usuario) => {
-             console.log('Error: ' + error);
-             console.log('idCliente: ' + idCliente);
-             console.log('Usuario: ' + usuario);
-             res.redirect('/Clientes');
-         })
-     })
-
-    // router.post('/ClientesEditar/Guardar/:id',(req, res) =>{
-    //     let id = req.params.id;
-        
-    //     Cliente.findByIdAndUpdate(id, {
-    //         nombreCliente: req.body.nombreCliente,
-    //         cedulaCliente: req.body.cedulaCliente,
-    //         direccionCliente: req.body.direccionCliente,
-    //         celularCliente: req.body.celularCliente,
-    //         correoCliente: req.body.correoCliente
-    //     }, (err, result) =>{
-    //         if(err){
-    //             res.json({ message: err.message, type: 'danger'});
-    //         }else{
-    //             req.session.message = {
-    //                 type: 'succes',
-    //                 message: 'User Updated Successfully!',
-    //             };
-    //             res.redirect('/Clientes');
-    //         }
-    //     })
-    // })
-
-  
-    router.get('/ClientesBorrar/:id', (req,res) => {
-        const eliminarCliente = (req.doby._id);
-        Cliente.findOneAndRemove(eliminarCliente, function(error){
-            if (error){
-              res.send(error);
-            }else{
-                res.redirect('/Clientes')
-            }
-        }) 
+    router.post('/ClientesEditar/Guardar/:id', (req,res) => {
+        const idCliente = (req.params.id);
+        console.log(idCliente);
+        console.log(req.body.nombreClienteEditar);
+        Cliente.findOneAndUpdate({_id: idCliente}, {
+            nombreCliente: req.body.nombreClienteEditar,
+            cedulaCliente: req.body.cedulaClienteEditar,
+            direccionCliente: req.body.direccionClienteEditar,
+            celularCliente: req.body.celularClienteEditar,
+            correoCliente: req.body.correoClienteEditar,
+        },{new: false}, (error, usuario) => {
+            // console.log('Error: ' + error);
+            // console.log('idCliente: ' + idCliente);
+            // console.log('Usuario: ' + usuario);
+            res.redirect('/Clientes');
+        })
     })
+
+
+    router.post('/ClientesEliminar/:id', async (req,res) => {
+         await Cliente.remove({ _id: req.params.id }, function(err, cliente) {
+            if (err)
+                res.send(err);
+            else
+                // res.json({ message: 'El cliente se ha eliminado' });
+                res.redirect('/Clientes')
+        });
+});
 
 
     router.post('/ClientesRegistro',  async(req,res) =>{
@@ -103,7 +86,9 @@ const { findByIdAndUpdate } = require('../models/Clientes');
         await newCliente.save();
             res.redirect('/Clientes');
     });
+    
 
+    //Registro Nuevo Usuario
     router.post('/Login',  async(req,res) =>{
         const newCliente = new Registro(req.body);
         await newCliente.save();
